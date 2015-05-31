45ff635920aacd7f060dacd86866450ffc6971fe
jMyles <justin@justinholmes.com>
2015-05-31 05:12:12 -0400

    Deprecating more comm_settings.

--- the_comm_app/call_functions.py
+++ the_comm_app/call_functions.py
@@ -3,49 +3,15 @@ Functions for extracting information from SIP / VOIP calls.
 Attempts to be less gnostic with providers and rely instead on the implementations in services.py
 '''
 
-import comm_settings
 from the_comm_app.models import PhoneCall, CommunicationInvolvement
-from django.contrib.auth.decorators import permission_required
-from django.db.models.signals import post_save
-from django.shortcuts import render
-from django.utils.datastructures import MultiValueDictKeyError
-
-# from what_apps.contact.models import PhoneNumber, PhoneProvider, DialList
-# from what_apps.do.models import Task, TaskPrototype, TaskRelatedObject, \
-#     TaskAccessPrototype, TaskAccess
-# from what_apps.people.models import UserProfile
-# from what_apps.utility.models import FixedObject
 
 import datetime
 import json
 import logging
 import re
 
-
-
-
-
-
-
-RESOLVE_PHONE_CALL = "TaskPrototype__resolve_phone_call" #TODO: There is an instance of this string in views.py.  Dehydrate.
-ANSWER_PHONE_CALLS_PRIVILEGE = 3
-
-
-
 comm_logger = logging.getLogger('comm')
 
-
-
-def get_or_create_nice_number(incoming_number):
-    '''
-    A wrapper over get_or_create for a PhoneNumber object.  Gets or Creates a new PhoneNumber from various formats.
-    '''
-    incoming_number = ''.join(number_as_list) #Now our incoming number is properly formatted.
-
-    phone_number = "+1" + str(incoming_number)
-    nice_number = phone_number[2:5] + "-" + phone_number[5:8] + "-" + phone_number[8:] #parse the number to look like django wants it: ex. 845-633-8330
-    phone_number_object, new = PhoneNumber.objects.get_or_create(number = nice_number)
-    return phone_number_object, new
 
 def call_object_from_call_info(call_info):
     '''
@@ -98,48 +64,3 @@ def call_object_from_call_info(call_info):
         call.ended = datetime.datetime.now()
 
     return call
-
-def place_conference_call_to_dial_list(call_id, dial_list_id):
-    dial_list = DialList.objects.get(id=dial_list_id)
-    call = PhoneCall.objects.get(id=call_id)
-    provider = call.service
-    active_numbers = dial_list.get_active_numbers() 
-    
-    for participant in active_numbers:  # Place a call to each active number
-        place_deferred_outgoing_conference_call(provider=provider, participant=participant, conference_id=call.id)
-    return True
-
-def proper_verbage_for_final_call_connection(call, response_object, announce_caller=False):
-    '''
-    Dehydration function for language to speak to picker-upper of a phone call.
-    
-    announce_caller is defaulted to true here to let the answerer know who is calling...lets fix the issue that requires this to be true.
-    
-    Takes a call and a generic response, appends the response with the appropriate say.
-    Returns True on success.
-    '''
-    final_warning = 'Connected. ' #Start constructing the final message to be delivered to the answerer.
-    
-    if announce_caller:
-        final_warning += call.announce_caller()
-        
-    current_participants = call.participants.filter(direction="to")
-    if current_participants: #If there are current participants, we want to make that clear to the answerer.
-        final_warning += 'Also on the call:'
-        for participant in current_participants:
-            final_warning += str(participant.person.first_name) + ','                
-        voice = "Victor"
-    else:
-        final_warning += comm_settings.SLASHROOT_EXPRESSIONS['first_answerer_alert']
-        voice = "Allison"
-        
-    response_object.say(final_warning, voice = voice)
-    return True
-
-@permission_required('comm.change_phonecall')
-def list_phone_calls_by_phone_number(request, phone_number_id):
-    phone_number = PhoneNumber.objects.get(id = phone_number_id)
-
-    calls = get_phone_calls_by_phone_number(phone_number)
-
-    return render(request, 'comm/list_phone_calls.html', locals())
